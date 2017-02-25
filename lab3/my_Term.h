7d4a2ffdc0b8fe2c8a37b3018f243b88fbebe48b
ProkopenkoRoman <long399@mail.ru>
2017-02-25 14:43:09 +0700

    Update my_Term.h

--- lab3/my_Term.h
+++ lab3/my_Term.h
@@ -8,22 +8,21 @@
 #include <limits.h>
 #include <unistd.h>
 
-enum colors {
-	clr_black,
-	clr_red,
-	clr_green,
-	clr_brown,
-	clr_blue,
-	clr_magenta,
-	clr_cyan,
-	clr_light_blue,
-	clr_default
+enum colors{
+	black,
+	red,
+	green,
+	yellow,
+	blue,
+	pink,
+	white
 };
 
-int mt_clrscr (void);
-int mt_gotoXY (int x, int y);
-int mt_getscreensize(int *rows, int *cols);
-int mt_setbgcolor(enum colors color);
+int mt_clrscr ();
+int mt_gotoXY(int x, int y);
+int mt_getscreensize (int *rows, int *cols);
+int mt_setfgcolor (enum colors color);
+int mt_setbgcolor (enum colors color);
 
 
 #endif
