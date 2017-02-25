6f230868e1216979c82ff48cc928b9042224c792
ProkopenkoRoman <long399@mail.ru>
2017-02-25 14:42:31 +0700

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -1,13 +1,24 @@
 #include "myTerm.h"
 
 int main(){
-	mt_clrscr ();
+	mt_clrscr (); //î÷èñòêà ýêðàíà
+	getchar();
 	
-	int r, c;
-	mt_gotoXY (10, 10);
-	mt_getscreensize(&r, &c);
-	//printf("r=%d c=%d", r, c);
-	//mt_setbgcolor(enum colors color);
+	int r=0, c=0;
+	mt_gotoXY (10, 10); //ïåðåâîä óêàçàòåëÿ â ïîçèöèþ 10,10
+	getchar();
 
+	mt_getscreensize(&r, &c); //ïîëó÷åíèå ðàçìåðà òåðìèíàëà
+	printf("rows=%d, cols=%d\n", r, c);
+	getchar();
+	
+	mt_setbgcolor(black); //óñòàíîâèì öâåò ôîíà íà áåëûé
+	mt_setfgcolor(red); //óñòàíîâèì öâåò ñèìâîëîâ íà æåëòûé
+	printf("IV-521, Prokopenko RP\n");
+	getchar();
+	
+	mt_setbgcolor(white); //óñòàíîâèì öâåò ôîíà íà áåëûé
+	mt_setfgcolor(black); //óñòàíîâèì öâåò ñèìâîëîâ íà æåëòûé
 	return 0;	
 }
+
