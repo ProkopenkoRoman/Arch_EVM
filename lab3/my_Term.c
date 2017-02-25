041924871407102cac870a4bc046d1405543e54f
ProkopenkoRoman <long399@mail.ru>
2017-02-25 14:42:48 +0700

    Update my_Term.c

--- lab3/my_Term.c
+++ lab3/my_Term.c
@@ -1,68 +1,92 @@
 #include "myTerm.h"
 
 int mt_clrscr (){
-	write(0, "\E[H\E[2J", 8);
+	write(1, "\E[H\E[2J", 8);
 	return 0;
 }
 
-int mt_gotoXY(int x, int y)
-{
-	int rows, cols;
-	mt_getscreensize(&rows, &cols);
-	if ((y < rows) && (x < cols) && (x >= 0) && (y >= 0)) {
-		//printf("\E[%d;%dH", y, x);
-		write(0, "\E[%d;%dH", 9);
-		return 0;
-	}
-	else
-		return -1;
+int mt_gotoXY(int x, int y){
+    char buf[15];
+    char *gotoxy = "\E[%d;%dH";
+
+    if (sprintf(buf, gotoxy, x, y) > 0) {
+	if (write(1, buf, strlen(buf)) == -1)
+	    return -1;
+    }
+    return 0;
 }
 
-int mt_getscreensize(int *rows, int *cols)
-{
-	struct winsize w;
-
-	if (!ioctl(STDOUT_FILENO, TIOCGWINSZ, &w)) {
-		*rows = w.ws_row;
-		*cols = w.ws_col;
-		return 0;
-	}
-	else
+int mt_getscreensize (int *rows, int *cols){
+    struct winsize size;
+    
+    if (ioctl(1, TIOCGWINSZ, (char *) &size) < 0){
+		perror("ioctl");
 		return -1;
-}
-/*
-int mt_setbgcolor(enum colors color)
-{
-	switch (color) {
-		case clr_black:
-			printf("\E[40m");
-			break;
-		case clr_red:
-			printf("\E[41m");
-			break;
-		case clr_green:
-			printf("\E[42m");
-			break;
-		case clr_brown:
-			printf("\E[43m");
-			break;
-		case clr_blue:
-			printf("\E[44m");
-			break;
-		case clr_magenta:
-			printf("\E[45m");
-			break;
-		case clr_cyan:
-			printf("\E[46m");
-			break;
-		case clr_light_blue:
-			printf("\E[47m");
-			break;
-		case clr_default:
-			printf("\E[49m");
-			break;
-		default:
-			return -1;
 	}
-	return 0;
-}*/
+	*rows = size.ws_row;
+    *cols = size.ws_col;
+
+    return 0;
+}
+
+int mt_setfgcolor (enum colors color){
+    char *color_text[] = {"\E[30m", "\E[31m", "\E[32m", "\E[33m", "\E[34m",  "\E[35m", "\E[37m"};
+    
+    switch(color) {
+	case black:
+	    write(1, color_text[0], strlen(color_text[0]));
+	    break;
+	case red:
+	    write(1, color_text[1], strlen(color_text[1]));
+	    break;
+	case green:
+	    write(1, color_text[2], strlen(color_text[2]));
+	    break;
+	case yellow:
+	    write(1, color_text[3], strlen(color_text[3]));
+	    break;
+	case blue:
+	    write(1, color_text[4], strlen(color_text[4]));
+	    break;
+	case pink:
+	    write(1, color_text[5], strlen(color_text[5]));       
+	    break;
+	case white:
+	    write(1, color_text[6], strlen(color_text[6]));       
+	    break;
+	default:
+	    printf("Color not found\n");
+	    break;
+    }
+}
+
+int mt_setbgcolor (enum colors color){
+    char *color_fons[] = {"\E[40m", "\E[41m", "\E[42m", "\E[43m", "\E[44m",  "\E[45m", "\E[47m"};
+    
+    switch(color) {
+	case black:
+	    write(1, color_fons[0], strlen(color_fons[0]));
+	    break;
+	case red:
+	    write(1, color_fons[1], strlen(color_fons[1]));
+	    break;
+	case green:
+	    write(1, color_fons[2], strlen(color_fons[2]));
+	    break;
+	case yellow:
+	    write(1, color_fons[3], strlen(color_fons[3]));
+	    break;
+	case blue:
+	    write(1, color_fons[4], strlen(color_fons[4]));
+	    break;
+	case pink:
+	    write(1, color_fons[5], strlen(color_fons[5]));       
+	    break;
+	case white:
+	    write(1, color_fons[6], strlen(color_fons[6]));       
+	    break;
+	default:
+	    printf("Color not found\n");
+	    break;
+    }
+}
