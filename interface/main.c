4d97ad63438e4c3a185cb92749f3466321b60498
ProkopenkoRoman <long399@mail.ru>
2017-03-04 11:20:14 +0700

    Update main.c

--- interface/main.c
+++ interface/main.c
@@ -1,7 +1,15 @@
 #include "interface.h" 
 int main(){
-	
-	print_interface();
 
+	sc_memoryInit();
+	sc_memorySet(0, 5);
+	sc_memorySet(1, 8);
+	sc_memorySet(2, 15);
+	sc_memorySet(2, 9999);
+	
+	print_frames();
+	printFlags();
+	printMemory();
+	
 	return 0; 
 }
